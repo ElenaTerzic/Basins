f86952760eb6424b2de50a6051913c9a5e3c4b3d
Arnau Miro <arnau.miro@upc.edu>
2021-05-25 17:51:31 +0200

    Added Balearic Sea from WorldSeas

--- Basins/worldseas.py
+++ Basins/worldseas.py
@@ -14,6 +14,7 @@ SHAPESPATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),'shapes')
 # World Seas shapefile
 adr    = Basin.from_npy('adr', 'Adriatic Sea'  , os.path.join(SHAPESPATH,'Adriatic_WorldSeas.npy'),downsample=4)
 alb    = Basin.from_npy('alb', 'Alboran Sea'   , os.path.join(SHAPESPATH,'Alboran_WorldSeas.npy'),downsample=4)
+bal    = Basin.from_npy('bal', 'Iberian Sea'   , os.path.join(SHAPESPATH,'Balearic_WorldSeas.npy'),downsample=4)
 
 socean = Basin.from_npy('so' , 'Southern Ocean', os.path.join(SHAPESPATH,'SouthernOcean_WorldSeas.npy'),downsample=4)
 
