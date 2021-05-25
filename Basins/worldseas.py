ced0531bb88db22c96045f9051559f4118e6d28b
Arnau Miro <arnau.miro@upc.edu>
2021-05-25 18:56:41 +0200

    Added Tyrrhenian from World Seas

--- Basins/worldseas.py
+++ Basins/worldseas.py
@@ -12,13 +12,14 @@ from .entities import Basin, ComposedBasin
 SHAPESPATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),'shapes')
 
 # World Seas shapefile
-adr    = Basin.from_npy('adr' , 'Adriatic Sea' , os.path.join(SHAPESPATH,'Adriatic_WorldSeas.npy'),downsample=4)
-alb    = Basin.from_npy('alb' , 'Alboran Sea'  , os.path.join(SHAPESPATH,'Alboran_WorldSeas.npy'),downsample=4)
-bal    = Basin.from_npy('bal' , 'Iberian Sea'  , os.path.join(SHAPESPATH,'Balearic_WorldSeas.npy'),downsample=4)
-lig    = Basin.from_npy('lig' , 'Ligurian Sea' , os.path.join(SHAPESPATH,'Ligurian_WorldSeas.npy'),downsample=4)
-west   = Basin.from_npy('west', 'Western Basin', os.path.join(SHAPESPATH,'WestMed_WorldSeas.npy'),downsample=4)
+adr    = Basin.from_npy('adr' , 'Adriatic Sea'  , os.path.join(SHAPESPATH,'Adriatic_WorldSeas.npy'),downsample=4)
+alb    = Basin.from_npy('alb' , 'Alboran Sea'   , os.path.join(SHAPESPATH,'Alboran_WorldSeas.npy'),downsample=4)
+bal    = Basin.from_npy('bal' , 'Iberian Sea'   , os.path.join(SHAPESPATH,'Balearic_WorldSeas.npy'),downsample=4)
+lig    = Basin.from_npy('lig' , 'Ligurian Sea'  , os.path.join(SHAPESPATH,'Ligurian_WorldSeas.npy'),downsample=4)
+tyr    = Basin.from_npy('tyr' , 'Tyrrhenian Sea', os.path.join(SHAPESPATH,'Tyrrhenian_WorldSeas.npy'),downsample=4)
+west   = Basin.from_npy('west', 'Western Basin' , os.path.join(SHAPESPATH,'WestMed_WorldSeas.npy'),downsample=4)
 
 socean = Basin.from_npy('so' , 'Southern Ocean', os.path.join(SHAPESPATH,'SouthernOcean_WorldSeas.npy'),downsample=4)
 
 # Composed basins
-wmed   = ComposedBasin('wmed', 'Western Mediterranean', [alb,west,bal,lig])
+wmed   = ComposedBasin('wmed', 'Western Mediterranean', [alb,west,bal,lig,tyr])
