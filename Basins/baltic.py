eabc8afdb72eec767193aca4d83aa0c2898ce3df
Arnau Miro <arnau.miro@upc.edu>
2022-08-20 18:25:22 +0200

    Fixed bug in basin definition

--- Basins/baltic.py
+++ Basins/baltic.py
@@ -9,7 +9,7 @@ import os, numpy as np
 from .entities import Basin, ComposedBasin
 
 bornholm = Basin.from_array('born', 'Bornholm', np.array([[15, 54.5 , 0.],   [17.8, 54.5 , 0.], [17.8, 55.5 , 0.], [15, 55.5 , 0.]])) 
-gdansk   = Basin.from_array('gdan', 'Gdansk'  , np.array([[18.1, 53.8, 0.],  [20., 53.8, 0.],   [20., 55.15, 0.],  [18.1, 53.8, 0.]])) 
+gdansk   = Basin.from_array('gdan', 'Gdansk'  , np.array([[18.1, 53.8, 0.],  [20., 53.8, 0.],   [20., 55.15, 0.],  [18.1, 55.15, 0.]])) 
 gotland  = Basin.from_array('gotl', 'Gotland' , np.array([[18.1, 55.15, 0.], [22., 55.15, 0.],  [22., 60., 0.],    [18.1, 60., 0.]])) 
 
 del print_function, division, os, np, Basin, ComposedBasin
