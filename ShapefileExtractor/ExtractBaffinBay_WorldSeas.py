2a3f1a904961386385981a45b0894c6cd4b0b02f
Arnau Miro <arnau.miro@upc.edu>
2023-02-23 11:52:54 +0100

    Adding a bunch of new basins from WorldSeas

---
+++ ShapefileExtractor/ExtractBaffinBay_WorldSeas.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+# Edited by amiro and eterzic 21.02.2021
+from __future__ import print_function, division
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import shapefile
+
+FILENAME = '/home/arnaumiro/projects/DATA/ShapeFiles/WorldSeas_v3/World_Seas_IHO_v3'
+
+sf  = shapefile.Reader(FILENAME)
+
+# Loop every lake
+for idx in range(len(sf)):
+	rec = sf.record(idx)
+	if 'Baffin' in rec[0]: print(idx,rec[0])
+
+# Extract Adriatic Sea
+print(sf.record(29))
+shp = sf.shape(29)
+xy  = np.array(shp.points)
+plt.plot(xy[:,0],xy[:,1],'.k')
+
+# Detect jump
+d    = np.diff(xy,axis=0)
+norm = np.sqrt(np.sum(d*d,axis=1))
+ids  = np.where(norm > 0.03*np.max(norm))[0]
+st   = ids[0]+1
+print(ids,st)
+plt.plot(xy[:st,0],xy[:st,1],'k')
+
+# Save data as xyz points
+xyz = np.zeros((st,3),dtype=np.double)
+xyz[:,0] = xy[:st,0]
+xyz[:,1] = xy[:st,1]
+
+np.save('../Basins/shapes/BaffinBay_WorldSeas.npy',xyz)
+
+plt.show()
---
+++ ShapefileExtractor/ExtractHudsonBay_WorldSeas.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+# Edited by amiro and eterzic 21.02.2021
+from __future__ import print_function, division
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import shapefile
+
+FILENAME = '/home/arnaumiro/projects/DATA/ShapeFiles/WorldSeas_v3/World_Seas_IHO_v3'
+
+sf  = shapefile.Reader(FILENAME)
+
+# Loop every lake
+for idx in range(len(sf)):
+	rec = sf.record(idx)
+	if 'Hudson' in rec[0]: print(idx,rec[0])
+
+# Extract Adriatic Sea
+print(sf.record(81))
+shp = sf.shape(81)
+xy  = np.array(shp.points)
+plt.plot(xy[:,0],xy[:,1],'.k')
+
+# Detect jump
+d    = np.diff(xy,axis=0)
+norm = np.sqrt(np.sum(d*d,axis=1))
+ids  = np.where(norm > 0.03*np.max(norm))[0]
+st   = ids[0]+1
+print(ids,st)
+plt.plot(xy[:st,0],xy[:st,1],'k')
+
+# Save data as xyz points
+xyz = np.zeros((st,3),dtype=np.double)
+xyz[:,0] = xy[:st,0]
+xyz[:,1] = xy[:st,1]
+
+np.save('../Basins/shapes/HudsonBay_WorldSeas.npy',xyz)
+
+plt.show()
