d11d6b0b695c7f0a4df7909b6af66e9ffe62883c
Arnau Miro <arnau.miro@upc.edu>
2023-02-24 11:19:56 +0100

    Baltic defined as simple rectangle

--- Basins/baltic.py
+++ Basins/baltic.py
@@ -6,10 +6,10 @@ from __future__ import print_function, division
 
 import os, numpy as np
 
-from .entities import Basin, ComposedBasin
+from .entities import Basin, ComposedBasin, SimpleRectangle
 
-bornholm = Basin.from_array('born', 'Bornholm', np.array([[15, 54.5 , 0.],   [17.8, 54.5 , 0.], [17.8, 55.5 , 0.], [15, 55.5 , 0.]])) 
-gdansk   = Basin.from_array('gdan', 'Gdansk'  , np.array([[20.0000000000000,55.141209644495056],[20.0000000000000,54.822843475681340],[19.5556640625000,53.800000000000000],[18.5394287109375,53.800000000000000],[18.1000000000000,54.822843475681340],[18.1000000000000,55.141209644495056]])) 
-gotland  = Basin.from_array('gotl', 'Gotland' , np.array([[18.1, 55.15, 0.], [22., 55.15, 0.],  [22., 60., 0.],    [18.1, 60., 0.]])) 
+bornholm = Basin('born', 'Bornholm', SimpleRectangle(15.0,17.8,54.5,55.5).points)
+gdansk   = Basin('gdan', 'Gdansk'  , SimpleRectangle(19.55,20.0,54.82,55.14).points)
+gotland  = Basin('gotl', 'Gotland' , SimpleRectangle(18.1,22,55.15,60).points)
 
-del print_function, division, os, np, Basin, ComposedBasin
+del print_function, division, os, np, Basin, ComposedBasin, SimpleRectangle
