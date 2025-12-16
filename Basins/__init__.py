42bcefac2e44c5cf6010d5f028649d4988453596
Arnau Miro <arnau.miro@upc.edu>
2025-12-16 10:05:08 +0100

    Rearranged

--- Basins/__init__.py
+++ Basins/__init__.py
@@ -2,10 +2,11 @@
 
 # Edited by amiro and eterzic 25.01.2021
 
-__version__ = '1.6.6'
+__version__ = '1.6.7'
 
 from .basic    import Point, Ball, Polygon
-from .entities import Basin, ComposedBasin, Line, SimpleRectangle, Rectangle, Plane, SimpleCube, Cube
+from .entities import Line, SimpleRectangle, Rectangle, Plane, SimpleCube, Cube, ConvexHull2D as ConvexHull
+from .entities import Basin, ComposedBasin
 from .         import generic, climate, hydrolakes, worldseas, worldcountries, adriatic, baltic, mediterranean
 
 del basic, entities
